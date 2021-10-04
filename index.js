54459ca06e663ec29623d820d9305a2de9b362a8
Julian <jhdevgrande@gmail.com>
2021-10-04 15:57:54 +0200

    feat(*): modify position

--- index.js
+++ index.js
@@ -2,12 +2,13 @@ import Buzon from './buzon.js';
 
 (function() {
   const persons = [
+    { name: 'Juli', avatar: './img/pacman2.png' },
     { name: 'Ricardo', avatar: './img/pacman2.png' },
+    { name: 'Estela', avatar: './img/pacman2.png' },
+    { name: 'Emilio', avatar: './img/pacman2.png' },
     { name: 'Jesús', avatar: './img/pacman2.png' },
     { name: 'Gian', avatar: './img/pacman2.png' },
     { name: 'Jorge', avatar: './img/pacman2.png' },
-    { name: 'Álvaro', avatar: './img/pacman2.png' },
-    { name: 'Juli', avatar: './img/pacman2.png' },
   ];
 
   const $mainImage = document.querySelector('#main-image');
